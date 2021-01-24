d744b68790b93d90ec52437415a86e9818c2d57c
Taikho <46679014+Taikho@users.noreply.github.com>
2021-01-25 00:33:48 +0100

    final_verision

--- Patryk_Reweda_151130/admin/admin.php
+++ Patryk_Reweda_151130/admin/admin.php
@@ -9,6 +9,7 @@ if ($_POST['login_email'] == 'admin' && $_POST['login_pass'] == 'admin') {
     $_SESSION['login_email'] = 'admin';
     echo 'Prawidłowe Hasło';
     echo dodajStrone();
+    echo usunStrone();
 }else {
     echo 'Wrong password';
      
@@ -46,6 +47,8 @@ if ($_POST['login_email'] == 'admin' && $_POST['login_pass'] == 'admin') {
 </html>
 
 <?php
+    
+include('../cfg/cfg.php');
 
 function dodajStrone(){
     $dod ='
@@ -55,15 +58,54 @@ function dodajStrone(){
 Id: <input type="Integer" name="id"><br>
 Strona_Tytułowa: <input type="text" name="page_title"><br>
 Zawartosc_Strony:<br><textarea rows="6" name="page_content" cols="40"></textarea><br>
-<input type="submit" name="submit" value="Submit">
+<input type="submit" name="dodaj" value="Dodaj">
 </form>
 ';
     return $dod;
     
 }
-    
-       
            
+function usunStrone(){
+    $us ='
+    <div class="container" style="margin-top:20px;">
+    <h2>Usun</h2>
+<form action="" method="post">
+Id: <input type="Integer" name="w_id"><br>
+<input type="submit" name="delete" value="Delete">
+</form>
+';
+    return $us;
+    
+}
+
+    
+if(isset($_POST['usun']))
+{
+    if (isset($_POST['w_id']))
+    {
+        $conn = polaczSie();
+        $sql = "DELETE FROM page_list WHERE id= ".$_POST['w_id'];
+        mysqli_query($conn, $sql);
+        
+    }
+}
+           
+if(isset($_POST['dodaj']))
+{
+  if(isset($_POST['id']) && isset($_POST['page_title']) && isset($_POST['page_content']))
+  {
+      $conn = polaczSie();
+      $sql = "INSERT INTO page_list (id, page_title, page_content)
+VALUES ('".$_POST["id"]."','".$_POST["page_title'"]."','".$_POST["page_content"]."')";
+      if ($conn->query($sql) === TRUE) {
+  echo "Gratulacje zostalo stworzone";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+      
+  }
+}
+    
            
            
      ?>
